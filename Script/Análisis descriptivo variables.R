817aff604c82d4140ecec3908fd72403d35ff685
mvgaona <mv.gaona@uniandes.edu.co>
2022-08-02 13:03:42 -0500

    Actualización descripción variables

--- Script/Análisis descriptivo variables.R
+++ Script/Análisis descriptivo variables.R
@@ -37,8 +37,32 @@ plot(hist(ONI),col = "purple", main="Histograma índice ONI desde 01/01/2000- 30
 min(ONI)
 max(ONI)
 mean(ONI)
-modeAT <- function(ONI){
+modeONI <- function(ONI){
   return(as.numeric(names(which.max(table(ONI)))))}
-modeTRM(ONI)
+modeONI(ONI)
 summary(ONI)
 ##----Generación por tipo de recursos----##
+GENCOMPLETO <- readRDS("C:/Users/norma/OneDrive/Escritorio/Valeria/Proyecto-final-MEcA-4107/Datos/Bases oficiales/GENCOMPLETO.rds")
+#-CoGenerador
+#Hora 0
+COGEN0<- GENCOMPLETO$Gen_CoGenerador0
+class(COGEN0)
+plot(hist(COGEN0),col = "purple", main="Histograma CoGenerador hora0 desde 01/01/2000- 30/06/2022",
+     xlab="CoGenerador0",
+     ylab="Frecuencia")
+min(COGEN0)
+max(COGEN0)
+mean(COGEN0)
+modeCOGEN0 <- function(COGEN0){
+  return(as.numeric(names(which.max(table(COGEN0)))))}
+modeCOGEN0(COGEN0)
+#Hora 1
+COGEN1<- GENCOMPLETO$Gen_CoGenerador1
+class(COGEN1)
+plot(hist(COGEN1),col = "purple", main="Histograma CoGenerador hora1 desde 01/01/2000- 30/06/2022",
+     xlab="CoGenerador 1",
+     ylab="Frecuencia")
+summary(COGEN1)
+modeCOGEN1 <- function(COGEN1){
+  return(as.numeric(names(which.max(table(COGEN1)))))}
+modeCOGEN1(COGEN1)
