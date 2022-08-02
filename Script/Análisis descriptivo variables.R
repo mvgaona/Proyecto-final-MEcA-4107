d7df864b9643ab890928878d1d15c763a14f90ed
mvgaona <mv.gaona@uniandes.edu.co>
2022-08-02 12:01:21 -0500

    Creación script análisis descriptivo

---
+++ Script/Análisis descriptivo variables.R
@@ -0,0 +1,5 @@
+# Valeria Gaona - 202214418
+# Andrea Beleño - 200620739
+
+#### TRABAJO FINAL #####
+BASEOF<- readRDS("C:/Users/valer/Desktop/Andes/Intersemestral/Big Data/Proyecto final/Proyecto-final-MEcA-4107/Datos/Bases oficiales/base_de_datos_oficial_3.rds")
